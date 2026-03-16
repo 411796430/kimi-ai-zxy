fc78ae2452ab5b7721f8438d8f9aec43d4d4e7d6
411796430 <31992336+411796430@users.noreply.github.com>
2026-03-16 19:03:28 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+};
+
+export default nextConfig;
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennextjs.org/cloudflare/bindings#local-access-to-bindings.
+// import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+// initOpenNextCloudflareForDev();
